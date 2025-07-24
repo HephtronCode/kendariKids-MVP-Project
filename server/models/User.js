1afeffd32702c4df3035f54a8d2b457bdea093c1
HephtronCode <bakr.innovator@gmail.com>
2025-07-24 23:41:55 +0100

    flip default role to teacher on signup

--- server/models/User.js
+++ server/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
 		role: {
 			type: String,
 			enum: ["admin", "teacher", "student", "staff", "parent"],
-			default: "student",
+			default: "teacher",
 		},
 		// Linked parent or guardian to their child or children
 		children: [
